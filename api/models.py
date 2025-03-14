6054f7c55a5c2c921885a209e67215418cc2a3fa
usamaamjad719@gmail.com <usamaamjad719@gmail.com>
2025-03-14 22:04:01 +0500

    hotfix: `Removed is_admin field from CustomUser model`

--- api/models.py
+++ api/models.py
@@ -52,7 +52,6 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
